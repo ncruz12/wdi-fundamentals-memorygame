45e15b7820fcca047f75ab02e75370edd5167340
nayeli cruz <nayelicruz12@gmail.com>
2017-07-11 20:01:42 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+//Holds all four cards
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardOne);
+//Holds cards that are in play
+var cardsInPlay = []
+
+//Represents the first card user flipped and storing the first element
+var cardOne = cards[0];
+
+//adding first card to cardsInPlay array
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen");
+
+//second card user flips and storing the third element in cards array to cardTwo
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped king");
+
+if(cardsInPlay.length === 2) {
+  if(cardsInPlay[0] === cardsInPlay[1] ) {
+    alert("You found a match!");
+  }
+  else {
+    alert("Sorry, try again.");
+  }
+
+}
+
